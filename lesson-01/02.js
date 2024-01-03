9433fa9c180596039c30178b40e3a08cafbf21c7
Anya Alesik <nayavii17@gmail.com>
2024-01-03 21:55:25 +0100

    added lesson1 02

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,10 @@
-// code
+let number1 = Math.floor(Math.random() * 100);
+
+switch (number1 % 2) {
+  case 0:
+    console.log(`${number1}:Это четное число`);
+    break;
+  case 1:
+    console.log(`${number1}:Это нечетное число`);
+    break;
+}
