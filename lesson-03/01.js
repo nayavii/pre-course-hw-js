3f653c8d605f61953f037b9249f538765533348c
Anya Alesik <nayavii17@gmail.com>
2024-01-28 10:09:40 +0100

    lesson-03 -- 01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,9 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter(function(element){
+      return element === 'черный' || element === 'красный' || element === 'желтый'
+    }).join('-');
+}
+
+console.log(createColorString());
